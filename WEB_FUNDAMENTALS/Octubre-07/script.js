3f2c4da47377b37ca33058fee0b66504ffb87214
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2024-10-07 23:50:07 -0500

    update

---
+++ WEB_FUNDAMENTALS/Octubre-07/script.js
@@ -0,0 +1,13 @@
+
+
+
+function handleLogoClick(){
+    console.log('Forza Ferrari');
+}
+
+
+function handleReadMore(element){
+    //element contiene el boton que se ha pulsado
+    element.style.backgroundColor = '#039b4e';
+    element.innerHTML = 'Forza Ferrari';
+}
